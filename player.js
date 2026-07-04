dc85d6650de1ae0d37fe06d6dc33fb9a69304632
chiru-1992 <chirugurung730@gmail.com>
2026-07-04 19:10:54 +0530

    Create player.js

---
+++ player.js
@@ -0,0 +1,327 @@
+const REVEAL_CODE = "VINILAAND";
+
+const tickets = [
+
+    {
+        id: 1,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 2,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 3,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 4,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 5,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 6,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 7,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 8,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 9,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 10,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 11,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 12,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 13,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 14,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 15,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 16,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 17,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 18,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 19,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 20,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 21,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 22,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    },
+
+    {
+        id: 23,
+        brazil: {
+            name: "",
+            number: "",
+            image: "players/"
+        },
+        norway: {
+            name: "",
+            number: "",
+            image: "players/"
+        }
+    }
+
+];
