34a3151bcfb7ac969200ecbc7a593cf51fbff507
chiru-1992 <chirugurung730@gmail.com>
2026-07-05 08:54:41 +0530

    Update player.js

--- player.js
+++ player.js
@@ -2,326 +2,146 @@ const REVEAL_CODE = "VINILAAND";
 
 const tickets = [
 
-    {
-        id: 1,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:1,
+brazil:{
+name:"Alex Sandro",
+image:"players/alex_sandro.jpg"
+},
+norway:{
+name:"Erling Haaland",
+image:"players/erling_haaland.jpg"
+}
+},
 
-    {
-        id: 2,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:2,
+brazil:{
+name:"Bremer",
+image:"players/bremer.jpg"
+},
+norway:{
+name:"Martin Odegaard",
+image:"players/martin_odegaard.jpg"
+}
+},
 
-    {
-        id: 3,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:3,
+brazil:{
+name:"Danilo",
+image:"players/danilo.jpg"
+},
+norway:{
+name:"Alexander Sorloth",
+image:"players/alexander_sorloth.jpg"
+}
+},
 
-    {
-        id: 4,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:4,
+brazil:{
+name:"Roger Ibanez",
+image:"players/roger_ibanez.jpg"
+},
+norway:{
+name:"Oscar Bobb",
+image:"players/osscar_bobb.jpg"
+}
+},
 
-    {
-        id: 5,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:5,
+brazil:{
+name:"Fabinho",
+image:"players/fabinho.jpg"
+},
+norway:{
+name:"Jorgen Strand Larsen",
+image:"players/jorgen_strand_larsen.jpg"
+}
+},
 
-    {
-        id: 6,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:6,
+brazil:{
+name:"Igor Thiago",
+image:"players/igor_thiago.jpg"
+},
+norway:{
+name:"Kristoffer Ajer",
+image:"players/kristoffer_ajer.jpg"
+}
+},
 
-    {
-        id: 7,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:7,
+brazil:{
+name:"Douglas Santos",
+image:"players/douglas_santos.jpg"
+},
+norway:{
+name:"Sander Berge",
+image:"players/sander_berge.jpg"
+}
+},
 
-    {
-        id: 8,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:8,
+brazil:{
+name:"Danilo Santos",
+image:"players/danilo_santos.jpg"
+},
+norway:{
+name:"Julian Ryerson",
+image:"players/julian_ryerson.jpg"
+}
+},
 
-    {
-        id: 9,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:9,
+brazil:{
+name:"Luiz Henrique",
+image:"players/luiz_herique.jpg"
+},
+norway:{
+name:"Antonio Nusa",
+image:"players/antonia_nusa.jpg"
+}
+},
 
-    {
-        id: 10,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:10,
+brazil:{
+name:"Leo Pereira",
+image:"players/leo_pereira.jpg"
+},
+norway:{
+name:"Jens Petter Hauge",
+image:"players/jens_petter_hauge.jpg"
+}
+},
 
-    {
-        id: 11,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
+{
+id:11,
+brazil:{
+name:"Gabriel Magalhaes",
+image:"players/gabriel_magalhaes.jpg"
+},
+norway:{
+name:"Marcus Holmgren Pedersen",
+image:"players/marcus_holmgren_pedersen.jpg"
+}
+},
 
-    {
-        id: 12,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 13,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 14,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 15,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 16,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 17,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 18,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 19,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 20,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 21,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 22,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    },
-
-    {
-        id: 23,
-        brazil: {
-            name: "",
-            number: "",
-            image: "players/"
-        },
-        norway: {
-            name: "",
-            number: "",
-            image: "players/"
-        }
-    }
-
-];
+{
+id:12,
+brazil:{
+name:"Rayan",
+image:"players/rayan.jpg"
+},
+norway:{
+name:"Kristian Thorstvedt",
+image:"players/kristian_thorstvedt.jpg"
+}
+},
