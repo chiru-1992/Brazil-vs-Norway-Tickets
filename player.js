e4cabff9b7541172316cdf24e7ac10fa5d1317ab
chiru-1992 <chirugurung730@gmail.com>
2026-07-05 09:15:41 +0530

    Add player.js with ticket data structure

---
+++ player.js
@@ -0,0 +1,17 @@
+const REVEAL_CODE = "VINILAAND";
+
+const tickets = [
+  {
+    id: 1,
+    brazil: {
+      name: "Alex Sandro",
+      image: "players/alex_sandro.jpg"
+    },
+    norway: {
+      name: "Erling Haaland",
+      image: "players/erling_haaland.jpg"
+    }
+  }
+
+  // More tickets...
+];
