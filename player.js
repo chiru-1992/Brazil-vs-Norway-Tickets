9fa00a10199be1f2427cd958a3f4dc42bdc019da
chiru-1992 <chirugurung730@gmail.com>
2026-07-05 09:20:10 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,17 +1,277 @@
 const REVEAL_CODE = "VINILAAND";
 
 const tickets = [
-  {
-    id: 1,
-    brazil: {
-      name: "Alex Sandro",
-      image: "players/alex_sandro.jpg"
-    },
-    norway: {
-      name: "Erling Haaland",
-      image: "players/erling_haaland.jpg"
-    }
-  }
 
-  // More tickets...
+{
+id:1,
+brazil:{
+name:"Alex Sandro",
+image:"players/alex_sandro.jpg"
+},
+norway:{
+name:"Erling Haaland",
+image:"players/erling_haaland.jpg"
+}
+},
+
+{
+id:2,
+brazil:{
+name:"Bremer",
+image:"players/bremer.jpg"
+},
+norway:{
+name:"Martin Odegaard",
+image:"players/martin_odegaard.jpg"
+}
+},
+
+{
+id:3,
+brazil:{
+name:"Danilo",
+image:"players/danilo.jpg"
+},
+norway:{
+name:"Alexander Sorloth",
+image:"players/alexander_sorloth.jpg"
+}
+},
+
+{
+id:4,
+brazil:{
+name:"Roger Ibanez",
+image:"players/roger_ibanez.jpg"
+},
+norway:{
+name:"Oscar Bobb",
+image:"players/oscar_bobb.jpg"
+}
+},
+
+{
+id:5,
+brazil:{
+name:"Fabinho",
+image:"players/fabinho.jpg"
+},
+norway:{
+name:"Patrick Berg",
+image:"players/patrick_berg.jpg"
+}
+},
+
+{
+id:6,
+brazil:{
+name:"Gabriel Martinelli",
+image:"players/gabriel_martinelli.jpg"
+},
+norway:{
+name:"Antonio Nusa",
+image:"players/antonia_nusa.jpg"
+}
+},
+
+{
+id:7,
+brazil:{
+name:"Bruno Guimaraes",
+image:"players/bruno_guimaraes.jpg"
+},
+norway:{
+name:"Kristoffer Ajer",
+image:"players/kristoffer_ajer.jpg"
+}
+},
+
+{
+id:8,
+brazil:{
+name:"Casemiro",
+image:"players/casemiro.jpg"
+},
+norway:{
+name:"Sander Berge",
+image:"players/sander_berge.jpg"
+}
+},{
+id:9,
+brazil:{
+name:"Gabriel Magalhaes",
+image:"players/gabriel_magalhaes.jpg"
+},
+norway:{
+name:"Julian Ryerson",
+image:"players/julian_ryerson.jpg"
+}
+},
+
+{
+id:10,
+brazil:{
+name:"Marquinhos",
+image:"players/marquinhos.jpg"
+},
+norway:{
+name:"Leo Ostigard",
+image:"players/leo_ostigard.jpg"
+}
+},
+
+{
+id:11,
+brazil:{
+name:"Lucas Paqueta",
+image:"players/lucas_paqueta.jpg"
+},
+norway:{
+name:"Jorgen Strand Larsen",
+image:"players/jorgen_strand_larsen.jpg"
+}
+},
+
+{
+id:12,
+brazil:{
+name:"Ederson Silva",
+image:"players/ederson_silva.jpg"
+},
+norway:{
+name:"Kristian Thorstvedt",
+image:"players/kristian_thorstvedt.jpg"
+}
+},
+
+{
+id:13,
+brazil:{
+name:"Douglas Santos",
+image:"players/douglas_santos.jpg"
+},
+norway:{
+name:"Fredrik Aursnes",
+image:"players/fredrik_aursnes.jpg"
+}
+},
+
+{
+id:14,
+brazil:{
+name:"Danilo Santos",
+image:"players/danilo_santos.jpg"
+},
+norway:{
+name:"Morten Thorsby",
+image:"players/morten_thorsby.jpg"
+}
+},
+
+{
+id:15,
+brazil:{
+name:"Leo Pereira",
+image:"players/leo_pereira.jpg"
+},
+norway:{
+name:"David Moller Wolfe",
+image:"players/david_moller_wolfe.jpg"
+}
+},
+
+{
+id:16,
+brazil:{
+name:"Igor Thiago",
+image:"players/igor_thiago.jpg"
+},
+norway:{
+name:"Jens Petter Hauge",
+image:"players/jens_petter_hauge.jpg"
+}
+},{
+id:17,
+brazil:{
+name:"Luiz Henrique",
+image:"players/luiz_herique.jpg"
+},
+norway:{
+name:"Marcus Holmgren Pedersen",
+image:"players/marcus_holmgren_pedersen.jpg"
+}
+},
+
+{
+id:18,
+brazil:{
+name:"Matheus Cunha",
+image:"players/matheus_cunho.jpg"
+},
+norway:{
+name:"Fredrik Andre Bjorkan",
+image:"players/fredrik_andre_bjorkan.jpg"
+}
+},
+
+{
+id:19,
+brazil:{
+name:"Rayan",
+image:"players/rayan.jpg"
+},
+norway:{
+name:"Andreas Schjelderup",
+image:"players/andreas_schjelderup.jpg"
+}
+},
+
+{
+id:20,
+brazil:{
+name:"Roger Ibanez",
+image:"players/roger_ibanez.jpg"
+},
+norway:{
+name:"Thelo Aasgaard",
+image:"players/thelo_aasgaard.jpg"
+}
+},
+
+{
+id:21,
+brazil:{
+name:"Endrick",
+image:"players/endrick.jpg"
+},
+norway:{
+name:"Torbjorn Heggem",
+image:"players/torbjorn_heggem.jpg"
+}
+},
+
+{
+id:22,
+brazil:{
+name:"Vinicius Junior",
+image:"players/vinicius_junior.jpg"
+},
+norway:{
+name:"Sondre Langas",
+image:"players/sondre_langas.jpg"
+}
+},
+
+{
+id:23,
+brazil:{
+name:"Neymer Jr",
+image:"players/neymer_jr.jpg"
+},
+norway:{
+name:"Hendrik Falchener",
+image:"players/hendrik_falchener.jpg"
+}
+}
+
 ];
